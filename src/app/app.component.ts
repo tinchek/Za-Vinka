1a5fd5a19b7c3fb423217f8beb6428f2e3306cc9
Tin Miletić Županić <tmzupanic@tis.hr>
2023-05-16 10:13:10 +0200

    api-komponenta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
   title = 'drugi-projekt';
-}
+ ngOnInit(): void {
+  let headers = new HttpHeaders({});
+ this.http.get<any>('https://api.openweathermap.org/data/2.5/weather?q={Zagreb}&appid={c7a86cc4dfc7b72fb150b9f9d0a74505}', {
+  headers: headers
+ }).subscribe(data => {
+  console.log(data);
+ })
+  };
+ }
+
---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherData(cityName: string) {
+    this.http.get(environment.weatherApiBaseUrl)
+  }
+}
